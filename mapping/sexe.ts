427c27d8a9a0bd1e030911542de49dcf5361631b
Wuraim <lorgnierg@yahoo.com>
2025-09-01 23:00:37 +0200

    0.0.5: ajout input + mapping sexe et skinGame

---
+++ mapping/sexe.ts
@@ -0,0 +1,6 @@
+import { CharacterSexe } from "../enums/sexe.ts";
+
+export const SEXE_RECORD: Record<number, CharacterSexe> = {
+  0x00: CharacterSexe.Male,
+  0x01: CharacterSexe.Female,
+};
---
+++ mapping/skinGame.ts
@@ -0,0 +1,6 @@
+import { CharacterSkinGame } from "../enums/skin.ts";
+
+export const RECORD_SKIN_GAME: Record<number, CharacterSkinGame> = {
+  0xfe: CharacterSkinGame.Dofus,
+  0xff: CharacterSkinGame.Wakfu,
+};
