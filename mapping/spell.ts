3434b031060f4bca9e9d555b70a56f3436e3412a
Wuraim <lorgnierg@yahoo.com>
2025-09-11 21:36:17 +0200

    0.0.11: #1 adding lacking mapping

--- mapping/spell.ts
+++ mapping/spell.ts
@@ -1,10 +1,17 @@
 import {
   CraSpell,
+  EcaflipSpell,
+  EniripsaSpell,
+  EnutrofSpell,
+  FecaSpell,
   IopSpell,
+  OsamodasSpell,
   PandawaSpell,
   RoublardSpell,
   SacrieurSpell,
   SadidaSpell,
+  SramSpell,
+  XelorSpell,
 } from "../enums/spell.ts";
 
 export const RECORD_IOP_SPELL: Record<number, IopSpell> = {
@@ -83,4 +90,95 @@ export const RECORD_ROUBLARD_SPELL: Record<number, RoublardSpell> = {
   0x1ba: RoublardSpell.Larcin,
   0x1c1: RoublardSpell.MauvaisOeil,
   0x1bb: RoublardSpell.Pulsar,
+};
+
+export const RECORD_ENIRIPSA_SPELL: Record<number, EniripsaSpell> = {
+  0x30: EniripsaSpell.Altruisme,
+  0x15: EniripsaSpell.MotCuratif,
+  0x19: EniripsaSpell.MotRevitalisant,
+  0x16: EniripsaSpell.MotSoignant,
+  0x17: EniripsaSpell.MotStimulant,
+  0xa1: EniripsaSpell.MotDeffacement,
+  0xa2: EniripsaSpell.MotDeJouvence,
+  0x85: EniripsaSpell.MotDeRegeneration,
+  0x18: EniripsaSpell.MotDeSacrifice,
+  0xa3: EniripsaSpell.MotDeTorture,
+};
+
+export const RECORD_SRAM_SPELL: Record<number, SramSpell> = {
+  0x49: SramSpell.AttaqueMortelle,
+  0x99: SramSpell.Brume,
+  0x3f: SramSpell.CoupSournois,
+  0x41: SramSpell.Diversion,
+  0x45: SramSpell.Double,
+  0x97: SramSpell.Invisibilite,
+  0x98: SramSpell.InvisibiliteDautrui,
+  0x43: SramSpell.Peur,
+  0x9a: SramSpell.PiegeMortel,
+  0x47: SramSpell.VolDeVie,
+};
+
+export const RECORD_XELOR_SPELL: Record<number, XelorSpell> = {
+  0x2b: XelorSpell.Aiguille,
+  0x9d: XelorSpell.CadranDeXelor,
+  0x96: XelorSpell.Contre,
+  0x2e: XelorSpell.Devouement,
+  0x2d: XelorSpell.FuiteDuTemps,
+  0x2c: XelorSpell.Horloge,
+  0x2f: XelorSpell.Momification,
+  0x9b: XelorSpell.PoussiereTemporelle,
+  0x2a: XelorSpell.Ralentissement,
+  0x9c: XelorSpell.VolDuTemps,
+};
+
+export const RECORD_ECAFLIP_SPELL: Record<number, EcaflipSpell> = {
+  0xc: EcaflipSpell.BondDuFelin,
+  0xb: EcaflipSpell.EspritFelin,
+  0xa0: EcaflipSpell.Fascination,
+  0xf: EcaflipSpell.Guigne,
+  0x9f: EcaflipSpell.Loterie,
+  0x9e: EcaflipSpell.Perception,
+  0xa: EcaflipSpell.PileOuFace,
+  0x86: EcaflipSpell.Roulette,
+  0xd: EcaflipSpell.ToutOuRien,
+  0xe: EcaflipSpell.Trefle,
+};
+
+export const RECORD_FECA_SPELL: Record<number, FecaSpell> = {
+  0x21: FecaSpell.ArmureAvalanche,
+  0x20: FecaSpell.ArmureOrageuse,
+  0x1f: FecaSpell.AttaqueNuageuse,
+  0x24: FecaSpell.BouclierFeca,
+  0x89: FecaSpell.Bulle,
+  0x23: FecaSpell.Faiblesse,
+  0x22: FecaSpell.Immunite,
+  0x8a: FecaSpell.Rebond,
+  0x8b: FecaSpell.Treve,
+  0x8c: FecaSpell.Teleportation,
+};
+
+export const RECORD_OSAMODAS_SPELL: Record<number, OsamodasSpell> = {
+  0x6e: OsamodasSpell.Bouftou,
+  0x37: OsamodasSpell.BenedictionAnimale,
+  0x8e: OsamodasSpell.Carapace,
+  0x3d: OsamodasSpell.Corbeau,
+  0x8f: OsamodasSpell.Craqueleur,
+  0x90: OsamodasSpell.CriDeLours,
+  0x39: OsamodasSpell.Fouet,
+  0x8d: OsamodasSpell.PiqureMotivante,
+  0x6f: OsamodasSpell.Prespic,
+  0x33: OsamodasSpell.Tofu,
+};
+
+export const RECORD_ENUTROF_SPELL: Record<number, EnutrofSpell> = {
+  0x27: EnutrofSpell.Acceleration,
+  0x93: EnutrofSpell.Corruption,
+  0x31: EnutrofSpell.ForceDeLage,
+  0x94: EnutrofSpell.Fossilisation,
+  0x26: EnutrofSpell.LancerDePiece,
+  0x25: EnutrofSpell.Maladresse,
+  0x95: EnutrofSpell.MaladresseDeMasse,
+  0x28: EnutrofSpell.PelleFantomatique,
+  0x29: EnutrofSpell.PelleMasacrante,
+  0x91: EnutrofSpell.TaniereDesRoches,
 };
