48ebf7c5bc6471619dd2b08ab62a61f428ff8ef6
Wuraim <lorgnierg@yahoo.com>
2025-09-13 23:39:46 +0200

    0.2.2: #2 avancement mapping equipment

--- mapping/equipment.ts
+++ mapping/equipment.ts
@@ -6,12 +6,22 @@ import {
   DofusEquipment,
 } from "../enums/equipment.ts";
 
-export const RECORD_WEAPON_EQUIPMENT: Record<number, WeaponEquipment> = {};
+export const RECORD_WEAPON_EQUIPMENT: Record<number, WeaponEquipment> = {
+  0xc: WeaponEquipment.Comete,
+};
 
-export const RECORD_PET_EQUIPMENT: Record<number, PetEquipment> = {};
+export const RECORD_PET_EQUIPMENT: Record<number, PetEquipment> = {
+  0xa7: PetEquipment.Abra,
+};
 
-export const RECORD_CAPE_EQUIPMENT: Record<number, CapeEquipment> = {};
+export const RECORD_CAPE_EQUIPMENT: Record<number, CapeEquipment> = {
+  0x6b: CapeEquipment.Abracapa,
+};
 
-export const RECORD_HEAD_EQUIPMENT: Record<number, HeadEquipment> = {};
+export const RECORD_HEAD_EQUIPMENT: Record<number, HeadEquipment> = {
+  0x73: HeadEquipment.Caracoiffe,
+};
 
-export const RECORD_DOFUS_EQUIPMENT: Record<number, DofusEquipment> = {};
+export const RECORD_DOFUS_EQUIPMENT: Record<number, DofusEquipment> = {
+  0x7d: DofusEquipment.Cawotte,
+};
