8e37dbaee0ea632708df02ccc5dbd3886e4d1ba3
Wuraim <lorgnierg@yahoo.com>
2025-09-18 00:36:01 +0200

    0.4.4: publish package to jsr

---
+++ main.ts
@@ -0,0 +1,6 @@
+import { parseAtd as _parseAtd } from "./parse.ts";
+import { convertTeamToAtd as _convertTeamToAtd } from "./unparse.ts";
+
+export const parseAtd = _parseAtd;
+
+export const convertTeamToAtd = _convertTeamToAtd;
