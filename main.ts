ec70b9a3a3037890a3a0ed5f2270e8c7b9664e3b
Wuraim <lorgnierg@yahoo.com>
2025-09-18 00:46:38 +0200

    0.4.5: Improving jsr package definition

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 import { parseAtd as _parseAtd } from "./parse.ts";
 import { convertTeamToAtd as _convertTeamToAtd } from "./unparse.ts";
 
-export const parseAtd = _parseAtd;
+/**
+ * Converts an ATD buffer (Uint8Array) to a team (Team).
+ * @param data - The ATD buffer to parse.
+ * @returns The team extracted from the buffer.
+ */
+export const convertAtdToTeam = _parseAtd;
 
+/**
+ * Converts a team (Team) to an ATD buffer (Uint8Array).
+ * @param team - The team to convert.
+ * @returns The ATD buffer corresponding to the team.
+ */
 export const convertTeamToAtd = _convertTeamToAtd;
